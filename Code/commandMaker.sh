1b3470557a540339daa235e640bf496972ee48ce
monkeyboy107 <isaackerley@gmail.com>
2016-09-17 00:19:50 -0600

    Internal Documentation

--- Code/commandMaker.sh
+++ Code/commandMaker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod +x minecraftServer
-sudo cp minecraftServer /usr/bin
+chmod +x minecraftServer #This permits the code to be executable
+sudo cp minecraftServer /usr/bin #This will make it an actual command that you can use
