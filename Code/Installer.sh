ca7e529f2d352903c24f7e08481292fd4a3a7b17
monkeyboy107 <isaackerley@gmail.com>
2016-09-17 00:11:37 -0600

    more
    Improved the installer to work better on ubuntu servers

--- Code/Installer.sh
+++ Code/Installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#This will add the java repository and install it 
-sudo add-apt-repository ppa:webupd8team/java
+#This will add the java repository and install it
+sudo apt-get install software-properties-common -y 
+sudo apt-get install python-software-properties -y  
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
 #This will create the path
