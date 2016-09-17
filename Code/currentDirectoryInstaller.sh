99e4f6c4e7ba685d9315cfa65fb1652e1a291850
Isaac <isaackerley@gmail.com>
2016-09-17 08:31:46 -0600

    Internal documentation

--- Code/currentDirectoryInstaller.sh
+++ Code/currentDirectoryInstaller.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+#########################
+#Programmer: Isaac Kerley
+#Date: 9/16/2016
+#Purpose: This will automatically make a minecraft bukkit server for anyone who wants to use it.
+#########################
+#Arrays
+#Variables
+########Main code########
 #This will add the java repository and install it
-sudo apt-get update
-sudo apt-get install software-properties-common -y 
-sudo apt-get install python-software-properties -y  
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update
-sudo apt-get install oracle-java8-installer -y
+sudo apt-get update #This will update all of the repos
+sudo apt-get install software-properties-common -y #This will permit me to use add-apt-repository
+sudo apt-get install python-software-properties -y #This will permit me to use add-apt-repository  
+sudo add-apt-repository ppa:webupd8team/java -y #This will add the webupd8team/java repository. This will permit installing Oracle's Java 
+sudo apt-get update #This will update all the repos. Permitting installing Oracle's Java
+sudo apt-get install oracle-java8-installer -y #This will install Oracle's Java version 8
 #This will download the jar file then run it initially
-wget https://www.getbukkit.org/files/craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar
-mv craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar minecraft-server.jar 
-java -jar minecraft-server.jar
+wget https://www.getbukkit.org/files/craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar #This will download minecraft server jar
+mv craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar minecraft-server.jar #This will rename the jar
+java -jar minecraft-server.jar #This will run the jar
