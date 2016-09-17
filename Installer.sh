1fee0b6b62c2a8f1d66718bd0dab869974cc4448
monkeyboy107 <isaackerley@gmail.com>
2016-09-16 22:23:44 -0600

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+#This will add the java repository and install it 
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
+#This will put the path in to place
 cd ~/Desktop
 mkdir minecraft
 cd minecraft
@@ -8,6 +11,7 @@ mkdir server
 cd server
 mkdir world01
 cd world01
-wget download-minecraft-server
-mv jar-name minecraft-server.jar
+#This will download the jar file then run it initially
+wget download-minecraft-server #Replace the download-minecraft-server with the path to the server
+mv jar-name minecraft-server.jar #Replace the jar-name with the jar name that will be on the path to the download
 java -jar minecraft-server.jar
