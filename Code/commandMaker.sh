0b0f11dbdcab2a0b08e4b00bcdb041fc5f4d0675
monkeyboy107 <isaackerley@gmail.com>
2016-09-17 01:15:38 -0600

    more

--- Code/commandMaker.sh
+++ Code/commandMaker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 chmod +x minecraftServer #This permits the code to be executable
 sudo cp minecraftServer /usr/bin #This will make it an actual command that you can use
+chmod +x minecraftServerShortcut #This permits the code to be executable
+sudo cp minecraftServerShortcut /usr/bin #This will make it an actual command that you can use
