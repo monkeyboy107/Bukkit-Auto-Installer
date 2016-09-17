f392cb77580a2e9da1236625598b12b856224cd2
Isaac <isaackerley@gmail.com>
2016-09-17 08:32:40 -0600

    Internal documentation

--- Code/commandMaker.sh
+++ Code/commandMaker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#########################
+#Programmer: Isaac Kerley
+#Date: 9/16/2016
+#Purpose: This will add the commands minecraftServer and minecraftServerShortcut
+#########################
+#Arrays
+#Variables
+########Main code########
 chmod +x minecraftServer #This permits the code to be executable
 sudo cp minecraftServer /usr/bin #This will make it an actual command that you can use
 chmod +x minecraftServerShortcut #This permits the code to be executable
