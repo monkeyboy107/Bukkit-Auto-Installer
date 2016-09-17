f890649a90df2bc24facf6085b591b4957bf7321
monkeyboy107 <isaackerley@gmail.com>
2016-09-16 23:22:06 -0600

    more
    improved the installer

--- Code/Installer.sh
+++ Code/Installer.sh
@@ -4,16 +4,12 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
 #This will put the path in to place
-cd ~
-mkdir minecraft
-cd minecraft
-mkdir Servers
-cd Servers
-mkdir default
-cd default
+mkdir ~/minecraft/Servers/default
 #This will download the jar file then run it initially
 wget download-minecraft-server #Replace the download-minecraft-server with the path to the server
 mv jar-name minecraft-server.jar #Replace the jar-name with the jar name that will be on the path to the download
-java -jar minecraft-server.jar
+cp minecraft-server.jar ~/minecraft/Servers/default
+del minecraft-server.jar
+java -jar ~/minecraft/Servers/default/minecraft-server.jar
 cp defaultMinecraftServer ~/minecraft/Servers/default
 sudo bash commandMaker.sh
