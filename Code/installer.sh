1d81726fb46432e432eb786f56ff1fd780039f61
Isaac <isaackerley@gmail.com>
2016-09-28 01:08:02 -0600

    Update installer.sh

--- Code/installer.sh
+++ Code/installer.sh
@@ -1,4 +1,3 @@
-@@ -1,5 +1,14 @@
 #!/bin/bash
 #########################
 #Programmer: Isaac Kerley
@@ -9,15 +8,21 @@
 #Variables
 ########Main code########
 #This will add the java repository and install it
-<<<<<<< HEAD
 sudo apt-get update
 sudo apt-get install software-properties-common -y 
 sudo apt-get install python-software-properties -y  
-@@ -15,7 +24,21 @@ mkdir ~/minecraft/Server/
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+clear
+wait 5
+#This will create the path
+mkdir ~/minecraft/
+mkdir ~/minecraft/Server/
+mkdir ~/minecraft/Server/
 sudo bash commandMaker.sh
 cd ~/minecraft/Server/
 clear
-=======
 sudo apt-get update #This will update all of the repos
 sudo apt-get install software-properties-common -y #This will permit me to use add-apt-repository
 sudo apt-get install python-software-properties -y #This will permit me to use add-apt-repository  
@@ -30,11 +35,7 @@ mkdir ~/minecraft/ #This will make the minecraft folder in the current user's ho
 mkdir ~/minecraft/Server/ #This will make the Server folder in the current user's home directory under minecraft
 sudo bash commandMaker.sh #This will start the commandMaker.sh script
 cd ~/minecraft/Server/ #this will go to the current user's home directory then minecraft then Server
->>>>>>> origin/master
 #This will download the jar file then run it initially
-wget https://www.getbukkit.org/files/craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar
-mv craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar minecraft-server.jar 
-java -jar minecraft-server.jar
 wget https://www.getbukkit.org/files/craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar #This will download minecraft server jar
 mv craftbukkit-1.10.2-R0.1-SNAPSHOT-latest.jar minecraft-server.jar #This will rename the jar
 minecraftServerShortcut #This will run the jar
