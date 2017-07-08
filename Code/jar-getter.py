769dcebc9aadf0cb63b1431c3a2e5f7e620c3dee
Isaac <isaackerley@gmail.com>
2017-07-07 22:36:23 -0600

    Adds a python script that gets the jar link

---
+++ Code/jar-getter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+#impots
+import requests
+import urllib
+
+class getWeb (object):
+    def getLink(self, url):
+        r = requests.session().get(url)
+        split = r.text.split('<a')
+        split = split[10]
+        split = str(split)
+        split = split.split('"')
+        url = split[1]
+        r.close()
+        return url
+    def getJar(self, url):
+        r = requests.session().get(url)
+        split = r.text.split('\n')
+        split = split[6]
+        split = str(split)
+        split = split.split('"')
+        url = split[3]
+        r.close()
+        return url
+
+def main():
+    url = "https://getbukkit.org/"
+    web = getWeb()
+    url = web.getLink(url)
+    jar = web.getJar(url)
+    print(jar)
+    exit()
+
+if __name__ == '__main__':
+    main()
