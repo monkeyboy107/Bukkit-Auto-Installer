c4fddda82f7649b69cfab1a02f8f0ccd800769f3
Isaac <isaackerley@gmail.com>
2017-07-09 07:36:59 -0600

    Delete dirMaker.sh

--- code/dirMaker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mkdir ~/minecraft/ #This will make the minecraft folder in the current user's home directory
-mkdir ~/minecraft/server/ #This will make the Server folder in the current user's home directory under minecraft
