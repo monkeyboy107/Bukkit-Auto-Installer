b22f76c1f27849c487a0695a1d7baf1ff7fa3ab0
Isaac Kerley <isaackerley@gmail.com>
2017-10-10 23:15:40 -0600

    more

--- code/Debian/installer.sh
+++ code/Debian/installer.sh
@@ -11,11 +11,11 @@ md=$(echo /opt/minecraft/server/)
 #######################################Main code###################################################################
                                                                                                                   #This will add the java repository and install it
 sudo apt-get update
-sudo apt-get install python-software-properties -y                                                                #This will permit me to use add-apt-repository
-sudo add-apt-repository ppa:webupd8team/java -y                                                                   #This will add the webupd8team/java repository. This will permit installing Oracle's Java 
+cd $gd
+cd Debian
+sudo bash java-adder 
 sudo apt-get update                                                                                               #This will update all the repos. Permitting installing Oracle's Java
-sudo echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections  #This will allow java to be installed without manually going through debconf
-sudo apt-get install oracle-java8-installer -y                                                                    #This will install Oracle's Java version 8
+sudo apt-get install oracle-java8-installer -y                                                                    #This will install Java
 sudo apt-get install python-pip -y                                                                                #This installs pip
 sudo apt-get install python-dev libssl-dev libffi-dev -y
 sudo pip install requests
@@ -23,7 +23,7 @@ sudo pip install pyopenssl ndg-httpsclient pyasn1
 python ../jar-getter.py                                                                                           #This will download the jar file then run it initially
                                                                                                                   #This will create the path
 bash ../extra-code/dir-maker.sh
-mv minecraft-server.jar /opt/minecraft/server
+mv minecraft-server.jar $md
 sudo bash ../command-maker.sh                                                                                     #This will start the commandMaker.sh script
 cd $md                                                                                                            #This will go to the current minecraft directory
 minecraftServerShortcut                                                                                           #This will run the jar
---
+++ code/Debian/java-adder.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main>>/etc/apt/sources.list.d/java-8-debian.list
+echo deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main>>/etc/apt/sources.list.d/java-8-debian.list
+
