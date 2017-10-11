ed419b2188d64e195f9e6460b23345408ac95ce3
Isaac Kerley <isaackerley@gmail.com>
2017-10-11 13:29:22 -0600

    more

--- code/extra-code/minecraft-server-download.sh
+++ code/extra-code/minecraft-server-download.sh
@@ -6,4 +6,4 @@ python ../jar-getter.py
 bash ../extra-code/dir-maker.sh
 mv minecraft-server.jar $md
 sudo bash chown.sh
-sudo bash chown minecraft:minecraft /opt/minecraft/minecraft-server.jar
+sudo chown minecraft:minecraft /opt/minecraft/server/minecraft-server.jar
